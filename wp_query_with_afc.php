34e57a8989c9f612e477016190a6b5c0fde9befd
arash-khd <126689940+arash-khd@users.noreply.github.com>
2023-06-09 15:01:06 +0330

    Update wp_query_with_afc.php

--- wp_query_with_afc.php
+++ wp_query_with_afc.php
@@ -1,15 +1,24 @@
-<?php
+<?php 
 $the_query = new WP_Query(
-array(
-    'meta_query'=>array(
-        array(
-            'key' => 'slider',
-            'value' => false,
-            'compare' => '!='
-    
-        )
+  array(
+  'meta_query'=>array(
+    array(
+      'key' => 'pro-post',
+      'value' => "1",
+      
     )
-    )
-    );
-    
-    ?>
+  
+  )
+  )
+  );
+
+  if ($the_query->have_posts()) {
+      while ($the_query->have_posts()) {
+		$the_query->the_post();
+		?>
+
+
+<?php 
+          
+      }
+          ?>
